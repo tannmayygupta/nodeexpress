3d81b03cb39ff31f0e23b1a622d641862aaa107d
tannmayygupta <guptatb_1@rknec.edu>
2025-02-01 13:43:56 +0530

    Learn all http method api push put get delete

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/adduser', (req, res) => {
+    res.send('user added')
+})
+
+app.put('/updateuser', (req, res) => {
+    res.send('Updated user')
+})
+
+app.delete('/deleteuser', (req, res) => {
+    res.send('Deleted user')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
